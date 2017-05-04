f41219110f8294ebdc073e2e79d61fde132ec86e
raix <nqmxt983@yahoo.co.jp>
2017-05-04 19:55:37 +0900

    v0.1.0 スプラッシュを導入

---
+++ app/src/main/java/net/snow69it/listeningworkout/SplashActivity.java
@@ -0,0 +1,26 @@
+package net.snow69it.listeningworkout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
